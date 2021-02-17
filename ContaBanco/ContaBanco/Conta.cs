f4b40f8e38522bf92afc6b0a1708477295200b15
Andressa Jovino S <andressajovinos@gmail.com>
2021-02-17 08:36:55 -0300

    Conta Banco

---
+++ ContaBanco/ContaBanco/Conta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBanco
+{
+	class Conta
+	{
+		public int Ag { get; set; }
+		public int Numero { get; set; }
+		public double Saldo { get; set; }
+
+		public Conta()
+		{
+		}
+
+		public Conta(int ag, int numero, double saldo)
+		{
+			Ag = ag;
+			Numero = numero;
+			Saldo = saldo;
+		}
+
+		public virtual void Saque(double quantia)
+		{
+			if (Saldo < quantia)
+				Console.WriteLine("Operacao invalida! ");
+			else
+				Saldo -= quantia;
+		}
+
+		public void Deposito(double quantia)
+		{
+			Saldo += quantia;
+		} 
+	}
+}
